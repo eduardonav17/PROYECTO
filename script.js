7d9138a4d410793e64f70f5a5916680ace70564d
juxn0616 <juanpablosifuentes24@cbtis52.edu.mx>
2026-03-05 19:37:15 -0600

    ya subimos a 11
    !!!!!!

--- script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector("#menu");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-
-abrir.addEventListener ("click", () => {
-    menu.classList.add("visible");
-})
