57463ce437e0851b211c2ee782a30d9cec527345
santitos095714 <ericksantos24@cbtis52.edu.mx>
2026-04-16 13:16:50 -0600

    Update registroarescatista.php

--- registroarescatista.php
+++ registroarescatista.php
@@ -6,89 +6,64 @@ require_once "header.php";
 <!--Contenido principal-->
 <main>
 
-<!--Titulo de la pagina-->
-<section class="titulo-seccion">
-   <h2 class="titulo">Registro rescatista</h2>
-</section>  
+<section class="contenido">
 
-<!--Seccion principal donde aparece el formulario-->
-<section class="hero-rescatista">
+<h2>Registro rescatista</h2>
 
-<section class="contenido-hero">
+<form id="formMascota">
 
-  <!--  FORMULARIO  -->
-  <section class="formulario">
+<!--Nombre-->
+<div class="grupo">
+<label>Nombre</label>
+<input type="text" id="nombre" placeholder="Ej. Juan Pablo" required>
+<span id="errorNombre" style="color:red;"></span>
+</div>
 
-  <!--Formulario de registro para rescatistas-->
-  <form action="#" method="POST" onsubmit="return false;"> <!-- aun no funciona -->
+<!--Nombre-->
+<div class="grupo">
+<label>Apellidos</label>
+<input type="text" id="Apellidos" placeholder="Ej. Sifuentes Ramirez" required>
+<span id="errorApellidos" style="color:red;"></span>
+</div>
 
-    <!--Campo nombre-->
-    <div class="form-group">
-      <label>Nombre *</label>
-      <input type="text" placeholder="Ej: Juan Pablo" name="nombre" required>
-    </div>
+<!--Raza-->
+<div class="grupo">
+<label>Edad</label>
+<input type="text" id="raza" placeholder="Ej. 67" required>
+<span id="errorEdad" style="color:red;"></span>
+</div>
 
-    <!--Campo apellidos-->
-    <div class="form-group">
-      <label>Apellidos *</label>
-      <input type="text" placeholder="Santos Santos" name="apellidos" required>
-    </div>
+<!--Color-->
+<div class="grupo">
+<label>Correo eletronico</label>
+<input type="text" id="correo" placeholder="Ej. petfriendshub230@gmail.com">
+</div>
 
-    <!--Campo edad-->
-    <div class="form-group">
-      <label>Edad *</label>
-      <input type="number" placeholder="16 años" name="edad" required>
-    </div>
+<!--Teléfono-->
+<div class="grupo">
+<label>Teléfono de contacto</label>
+<input type="tel" id="telefono" placeholder="3510000000" required>
+<span id="errorTelefono" style="color:red;"></span>
+</div>
 
-    <!--Correo electronico-->
-    <div class="form-group">
-      <label>Correo electrónico *</label>
-      <input type="email" placeholder="juanpablo046540@gmail.com" name="correo" required>
-    </div>
+<!--Foto-->
+<div class="grupo">
+<label>Idetificacion</label>
+<input type="file" id="foto" accept="image/*" required>
+</div>
 
-    <!--Telefono celular-->
-    <div class="form-group">
-      <label>Celular *</label>
-      <input type="tel" placeholder="351-000-0000" name="celular" required>
-    </div>
+<!--Botón-->
+<button type="submit" class="pub">Publicar Adopción</button>
 
-    <!--Cantidad de perros-->
-    <div class="form-group">
-      <label>¿Cuántos perros tienes a tu cargo? *</label>
-      <input type="number" name="cantidad_perros" required>
-    </div>
-
-    <!--Subir identificacion-->
-    <div class="form-group">
-      <label>Identificación oficial *</label>
-      <input type="file" name="identificacion" required>
-    </div>
-
-    <!--Subir comprobante de domicilio-->
-    <div class="form-group">
-      <label>Comprobante de domicilio *</label>
-      <input type="file" name="comprobante" required>
-    </div>
-
-    <!--Boton para enviar formulario-->
-    <button type="submit" class="btn-enviar">Enviar</button>
-
-  </form>
-
-  </section>
+</form>
 
 </section>
-
-</section>
-
-</section>
-
-
 
 </main>
 
+<script src="script.js"></script>
 <!--Pie de pagina-->
 <?php include("footer.php"); ?>
   
 </body>
-</html>
+</html>    
